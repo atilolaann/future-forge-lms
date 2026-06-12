afa2a4cd83ac3267ea4369384b1bb206789ea25e
heartyJ <atilolaanna@gmail.com>
2026-06-12 10:34:26 +0100

    sidebar changes

--- src/components2/Sidebar.jsx
+++ src/components2/Sidebar.jsx
@@ -9,14 +9,11 @@ import Vector4 from '../assets/Vector4.png'
 function Sidebar (){
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
     return (
-        <div className={' border-r border-gray-300 transition-all duration-300 ${isSidebarOpen ? "w-56": "w-30"}'}>
+        <div className={' border-r border-gray-300 transition-all duration-300 ${isSidebarOpen ? "w-56": "w-16"}'}>
             <div className='p-8  max-w-[50] flex flex-col gap-12 min-h-screen'>
                <button onClick={() => setIsSidebarOpen(!isSidebarOpen)}
                > 
                 <img src={List}  className='w-5 h-5' alt=" " /></button>
-                
-
-
                 <div className='flex flex-col gap-4'>
 
                          <Link to="/Dashboard">
@@ -31,20 +28,12 @@ function Sidebar (){
                           {isSidebarOpen && <p> Recordings</p> }
                     </div>
                          </Link>
-
-                        
-                    
                          <Link to="/Resourcescontent">
                          <div className="flex items-center gap-3  py-2 hover:px-8   rounded-lg hover:bg-orange-400 hover:text-white transition-all duration-200 cursor-pointer">
                            <img src={Vector3} className='w-5 h-5 hover:fill-white' alt="" />
                             {isSidebarOpen && <p>Resources</p> }
                     </div>
                          </Link>
-                    
-                    
-                         
-                    
-                  
                         <Link to="/Account">
                           <div className="flex items-center gap-3  py-2 hover:px-8   rounded-lg hover:bg-orange-400 hover:text-white transition-all duration-200 cursor-pointer">
                            <img src={Vector4} className='w-5 h-5' alt="" />
