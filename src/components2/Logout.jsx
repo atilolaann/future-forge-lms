6312cd4d40db701a0bafda15d300164221721183
heartyJ <atilolaanna@gmail.com>
2026-06-12 17:20:13 +0100

    made adjustment on logout again

--- src/components2/Logout.jsx
+++ src/components2/Logout.jsx
@@ -6,9 +6,9 @@ function Logout({setShowLogout}) {
  
 
   return (
-    <div className="bg-orange-400 rounded-md flex m-4 p-2 max-w-[200] ">
+    <div className="bg-orange-400 rounded-md flex m-4 p-2 max-w-[200] hover:bg-gray-700  cursor-pointer">
       <button
-       className="text-white flex items-center gap-3 hover:bg-gray-700 p-2 rounded cursor-pointer"
+       className="text-white flex items-center gap-3 p-2"
        onClick={() =>setShowLogout(true)}>
           <img src={SignOut} className="w-5 h-5" alt="" />
            <p>Logout</p>
