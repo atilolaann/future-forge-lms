3b1f89d1a1be742545b4c8ce1ea41a00a335f9a4
heartyJ <atilolaanna@gmail.com>
2026-06-13 12:50:31 +0100

    sidebar changes

--- src/components2/Sidebar.jsx
+++ src/components2/Sidebar.jsx
@@ -60,10 +60,10 @@ function Sidebar() {
             </div>
           </Link>
           <Link to="/Recordingscontent">
-            <div className={`flex items-center  rounded-lg transition-all duration-200 cursor-pointer
+            <div className={`flex items-center justify-center rounded-lg transition-all duration-200 cursor-pointer
                 ${isSidebarOpen?
                  "gap-3 px-3 py-2 hover:px-8 hover:bg-orange-400 hover:text-white"
-                :"justify-center p-3 hover:bg-orange-400"}`}>
+                :"justify-center py-3 hover:bg-orange-400"}`}>
              <div className= "flex items-center gap-2">
                <div  className="w-5 h-5">
                 <img
