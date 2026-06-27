13103207e0399b7f643d4f0334ffdf4f7557468e
heartyJ <atilolaanna@gmail.com>
2026-06-27 07:03:35 +0100

    fixed some bugs on the dashboard

--- src/components2/Welcomecard.jsx
+++ src/components2/Welcomecard.jsx
@@ -1,5 +1,5 @@
 import radial from "../assets/radial.png";
-function Welcomecard() {
+function Welcomecard({ userData }) {
   return (
     <div className="w-full ">
       <div className="m-0 sm:m-2 p-4 sm:p-6 flex flex-col sm:flex-row items-center justify-between w-full h-full rounded-lg shadow-inner hover:-translate-y-3 hover:shadow-lg transition-all duration-300">
@@ -7,9 +7,9 @@ function Welcomecard() {
           <h3 className="py-2 sm:py-4 text-lg sm:text-xl font-bold">
             Week 9/20
           </h3>
-          <p className="text-sm sm:text-base">9 of 10 lessons completed</p>
+          <p className="text-sm sm:text-base">9 of 20 lessons completed</p>
           <p className="text-sm sm:text-base mt-1 sm:mt-0">
-            Frontend Development Track
+            {userData?.trackId?.name}
           </p>
         </div>
         <div className="flex flex-col items-center mt-4 sm:mt-0 text-center">
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -67,7 +67,7 @@ function Dashboard() {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 items-stretch">
-            <Welcomecard />
+            <Welcomecard userData={userData} />
             <Progresschart />
           </div>
 
