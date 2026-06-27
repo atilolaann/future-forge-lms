22d4b2443b52595336af66b39e2defc562b42873
heartyJ <atilolaanna@gmail.com>
2026-06-27 08:34:11 +0100

    fixed sessiontime out issue

--- src/components2/SessionTimeout.jsx
+++ src/components2/SessionTimeout.jsx
@@ -1,11 +1,15 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "sonner"
 
 function SessionTimeout() {
   const navigate = useNavigate();
 
   useEffect(() => {
     let timeout;
+    console.log("SessionTimeout mounted");
+
+    
 
     const resetTimer = () => {
       
@@ -13,12 +17,14 @@ function SessionTimeout() {
 
       timeout = setTimeout(
         () => {
+          console.log("Timer expired");
           localStorage.removeItem("token");
+          toast.error("Session expired. Please log in again")
 
           alert("Session expired. Please login again.");
           navigate("/login");
         },
-        15 * 60 * 1000,
+        15* 20 * 1000,
       ); // 20 minutes
     };
 
